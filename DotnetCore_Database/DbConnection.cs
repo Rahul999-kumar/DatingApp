f2e8d8e5d8603ff69e7cab4f99decb621fd5fbfe
rahul999.kumar <rahul999.kumar@gmail.com>
2022-08-20 22:20:28 +0530

    First Commit

---
+++ DotnetCore_Database/DbConnection.cs
@@ -0,0 +1,19 @@
+﻿using DotnetCore_Database.DbEntities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetCore_Database
+{
+    public class DbConnection : DbContext
+    {
+        public DbConnection(DbContextOptions<DbConnection> options) : base(options)
+        {
+
+        }
+        public DbSet<Users> Users { get; set; }
+    }
+}
---
+++ DotnetCore_Database/DbEntities/Users.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetCore_Database.DbEntities
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+    }
+}
